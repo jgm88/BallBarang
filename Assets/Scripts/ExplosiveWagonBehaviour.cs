65a7e0153ba849c9bbcfc161d8962a33531d225c
Salva <sgm69@alu.ua.es>
2015-01-04 21:08:55 +0100

    movido script de comportamiento de la vagoneta a la carpeta scripts

--- Assets/Scripts/ExplosiveWagonBehaviour.cs
+++ Assets/Scripts/ExplosiveWagonBehaviour.cs
@@ -15,7 +15,7 @@ public class ExplosiveWagonBehaviour : MonoBehaviour {
 	IEnumerator movementCor()
 	{
 		float smooth = 0.7f;
-		Vector3 movPos = new Vector3 (this.transform.position.x,this.transform.position.y,this.transform.position.z);
+//		Vector3 movPos = new Vector3 (this.transform.position.x,this.transform.position.y,this.transform.position.z);
 
 		while (Vector3.Distance(this.transform.position,destiny)>0.05f) {
 			transform.position= Vector3.Lerp(this.transform.position,destiny,smooth*Time.deltaTime);
