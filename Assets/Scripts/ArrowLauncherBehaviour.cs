e70be703608014c8adb4509477031dcf313ae393
jgm79 <jgm79@alu.ua.es>
2015-01-05 16:45:12 +0100

    Anyadidos prefabs y scrits comportamiento de Arrow, Fence y Coin

---
+++ Assets/Scripts/ArrowLauncherBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowLauncherBehaviour : MonoBehaviour {
+
+	public GameObject arrowPrefab;
+	public Transform arrowEnd;
+	public float force = 500f;
+	public float delay = 3f;
+	GameObject arrow;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(shotArrow());
+
+	}
+	IEnumerator shotArrow(){
+		while(true){
+
+			arrow = Instantiate(arrowPrefab,transform.position, Quaternion.identity) as GameObject;
+			arrow.transform.Rotate(0f,-90f,0f);
+			arrow.rigidbody.AddForce(force,0f,0f);
+			arrow.rigidbody.AddTorque(1000f,0f,0f);
+			yield return new WaitForSeconds (delay);
+		}
+	}
+}
--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class CoinBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float rotationSpeed = 1.5f;
+	public GameObject fence;
+	private FenceBehaviour fenceBehaviour;
+
+	void Start(){
+		fenceBehaviour = fence.GetComponent<FenceBehaviour>();
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0f, 1f, 0f);
+		transform.Rotate(0f, rotationSpeed, 0f);
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			fenceBehaviour.tryOpen();
+			GameObject.Destroy(gameObject);
+		}
 	}
 }
