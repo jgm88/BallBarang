ce003be1dc156e272340ddfb303f26e1e890d9b2
jgm79 <jgm79@alu.ua.es>
2015-01-05 19:37:18 +0100

    Ajustados sistemas de particulas al prefab Coin y anyadido comportamiendo a coinBehaviour

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -5,10 +5,17 @@ public class CoinBehaviour : MonoBehaviour {
 
 	public float rotationSpeed = 1.5f;
 	public GameObject fence;
+
 	private FenceBehaviour fenceBehaviour;
+	private ParticleSystem particles1;
+	private ParticleSystem particles2;
+	private GameObject coinModel;
 
 	void Start(){
 		fenceBehaviour = fence.GetComponent<FenceBehaviour>();
+		particles1 = transform.GetChild(0).GetComponent<ParticleSystem>();
+		particles2 = transform.GetChild(1).GetComponent<ParticleSystem>();
+		coinModel = transform.GetChild(2).gameObject;
 	}
 	// Update is called once per frame
 	void Update () {
@@ -16,8 +23,14 @@ public class CoinBehaviour : MonoBehaviour {
 	}
 	void OnTriggerEnter(Collider other){
 		if(other.tag == "Player"){
+			//Hacemos desaparecer la moneda
+			gameObject.collider.enabled = false;
+			coinModel.SetActive(false);
+
+			particles1.Play();
+			particles2.Play();
+
 			fenceBehaviour.tryOpen();
-			GameObject.Destroy(gameObject);
 		}
 	}
 }
