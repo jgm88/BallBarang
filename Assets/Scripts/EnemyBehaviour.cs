a94a323b8f71ea30168554601d8c5285ab3d880a
jgm79 <jgm79@alu.ua.es>
2015-01-16 00:43:48 +0100

    Anyadidos menus Gameover, Esc | EnemyBehaviour llama a deduct life del player | terminadas mecanicas de vida | anadidos sonidos de golpe

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	
+	public int damage = 1;
+	private PlayerBehaviour playerBehaviour;
+
+	// Use this for initialization
+	void Start () {
+		playerBehaviour = GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>();
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player")
+			playerBehaviour.deductLife(damage);
+	}
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,19 +4,27 @@ using System.Collections;
 public class PlayerBehaviour : MonoBehaviour {
 
 	public int life = 3;
+	private Transform[] vHearts;
+	private MeshRenderer mesh;
+
 	// Use this for initialization
 	void Start () {
-	
+		vHearts = GameObject.Find("Hearts").GetComponentsInChildren<Transform>();
+
 	}
 
 	public void deductLife(int damage){
 		life -= damage;
+		vHearts[life].gameObject.SetActive(false);
+		audio.Play();
+
 		if(life <= 0)
 			die();
 	}
 
 	private void die(){
-		//TODO
-		//Comportamiento de muerte
+		gameObject.GetComponent<PlayerMovement>().enabled = false;
+		mesh = gameObject.GetComponentInChildren<MeshRenderer>();
+		mesh.enabled = false;
 	}
 }
