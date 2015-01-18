522633f534ae9bcaa43b00679e4a85014b7afe2a
unknown <C-sar Snake>
2015-01-18 18:03:14 +0100

    Bug Arreglado

--- Assets/Scripts/CameraBeachBehaviour.cs
+++ Assets/Scripts/CameraBeachBehaviour.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class CameraBeachBehaviour : MonoBehaviour {
 	private float initialDistance;
-	private float currentDistance;
+//	private float currentDistance;
 	// Use this for initialization
 	void Start(){
 		
 		initialDistance = Vector3.Distance(transform.position, transform.parent.position);
-		currentDistance = initialDistance;
+		//currentDistance = initialDistance;
 	}
 	
 	// Update is called once per frame
