b17e37afc6d22842a55b4573a6ff3ebc67e6a38d
Luis <luisetepe6@gmail.com>
2015-01-19 22:17:46 +0100

    FINISHED

--- Assets/Scripts/FinalDoorBehaviour.cs
+++ Assets/Scripts/FinalDoorBehaviour.cs
@@ -9,11 +9,13 @@ public class FinalDoorBehaviour : MonoBehaviour {
 	public GameObject rayoRojo;
 	public GameObject rayoVerde;
 	public GameObject rayoAmarillo;
-
+	public BoxCollider door;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 	
+		count = 0;
 	}
 	
 	void QuitColor(DoorColors color)
@@ -22,19 +24,28 @@ public class FinalDoorBehaviour : MonoBehaviour {
 		{
 			rayoAmarillo.GetComponent<LineRenderer>().enabled=false;
 			rayoAmarillo.GetComponent<Lightning>().enabled = false;
+			count++;
 		}else if (color == DoorColors.VERDE)
 		{
 			rayoVerde.GetComponent<LineRenderer>().enabled=false;
 			rayoVerde.GetComponent<Lightning>().enabled = false;
+			count++;
 		}else if (color == DoorColors.AZUL)
 		{
 			rayoAzul.GetComponent<LineRenderer>().enabled=false;
 			rayoAzul.GetComponent<Lightning>().enabled = false;
+			count++;
 		}else if (color == DoorColors.ROJO)
 		{
 			rayoRojo.GetComponent<LineRenderer>().enabled=false;
 			rayoRojo.GetComponent<Lightning>().enabled = false;
+			count++;
 		}
+
+		if (count == 4)
+			door.enabled = false;
+
+
 
 	}
 }
