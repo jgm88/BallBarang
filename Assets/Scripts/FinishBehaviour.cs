030c50461114378717756a19d072e201c23efc91
Luis <luisetepe6@gmail.com>
2015-01-19 20:37:55 +0100

    Futuristic Scene terminada

---
+++ Assets/Scripts/FinishBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishBehaviour : MonoBehaviour {
+
+	private GameObject winpanel;
+	// Use this for initialization
+	void Awake () {
+	
+		winpanel = GameObject.Find ("WinPanel");
+		winpanel.SetActive (false);
+	}
+	
+
+
+	void OnTriggerEnter(Collider colider)
+	{
+
+		if (colider.tag == "Player") {
+			winpanel.SetActive(true);
+			GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic=true;
+			GameObject.Find ("EventSystem").GetComponent<GlobalEventSystemBehaviour> ().showCursor (true);
+		}
+
+	}
+}
